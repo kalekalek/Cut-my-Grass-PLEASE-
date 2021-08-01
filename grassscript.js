41b8f241e32b68ea3de97668ca4d9428a6518fb8
kalekalek <87471596+kalekalek@users.noreply.github.com>
2021-08-01 12:09:56 -0400

    fixed one bracket

--- grassscript.js
+++ grassscript.js
@@ -7,16 +7,16 @@ function openPage(pageName, elmnt, color) {
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
+  
   // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
-
+  
   // Show the specific tab content
   document.getElementById(pageName).style.display = "block";
-
+  
   // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = color;
 }
@@ -32,27 +32,20 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-function validateForm() {
-var x = document.forms["myForm"]
-[""].value;
-if (x == "") {
-alert("Service must be chosen");
-return false;
+// new quote
 
-}
-}
 let button = document.getElementById('button');
 let output = document.getElementById('output');
 let quotes =[
-'"Cutting Grass is cool."- Cut My Grass Please!! ',
-'"Where did you learn those skills!!" - Cut My Grass PLEASE!!!',
-'"Why did the lawnmower cross the road? To" - Cut My Grass PLEASE!!!',
-'"What is your favorite type of grass?" - Cut My Grass PLEASE!!!',
-'"What did the pushmower say to the riding mower? - Cut My Grass PLEASE!!!',
+  '"Cutting Grass is cool."- Cut My Grass Please!! ',
+  '"Where did you learn those skills!!" - Cut My Grass PLEASE!!!',
+  '"Why did the lawnmower cross the road? To" - Cut My Grass PLEASE!!!',
+  '"What is your favorite type of grass?" - Cut My Grass PLEASE!!!',
+  '"What did the pushmower say to the riding mower? - Cut My Grass PLEASE!!!',
 ];
 button.addEventListener('click', function()
 {
-
+  
   var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
   output.innerHTML = randomQuote;
 })
